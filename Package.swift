a7e4f36c73236f80cdecaacbc5c080c868008a7e
Oliver Foggin <oliverfoggin@me.com>
2023-09-05 13:26:20 +0100

    Update to TCA 1.0.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
 		.library(name: "ComposableAnalytics", targets: ["ComposableAnalytics"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/pointfreeco/swift-composable-architecture", branch: "release/1.0"),
+		.package(url: "https://github.com/pointfreeco/swift-composable-architecture", .upToNextMajor(from: "1.0.0")),
 	],
 	targets: [
 		.target(
