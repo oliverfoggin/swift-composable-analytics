b1e1c0fb7046582fe6ffe28b15cc55c364fa9ba6
Oliver Foggin <oliverfoggin@me.com>
2023-11-09 15:10:17 +0000

    New reducer

--- Sources/ComposableAnalytics/MultipleAnalyticsReducer.swift
+++ Sources/ComposableAnalytics/MultipleAnalyticsReducer.swift
@@ -2,31 +2,33 @@ import Foundation
 import ComposableArchitecture
 
 @available(*, deprecated)
-public struct AnalyticsReducer<State, Action>: Reducer {
+public struct MultipleAnalyticsReducer<State, Action>: Reducer {
 	@usableFromInline
-	let toAnalyticsData: (State, Action) -> AnalyticsData?
-	
+	let toAnalyticsData: (State, Action) -> [AnalyticsData]?
+
 	@usableFromInline
 	@Dependency(\.analyticsClient) var analyticsClient
-	
+
 	@inlinable
-	public init(_ toAnalyticsData: @escaping (State, Action) -> AnalyticsData?) {
+	public init(_ toAnalyticsData: @escaping (State, Action) -> [AnalyticsData]?) {
 		self.init(toAnalyticsData: toAnalyticsData, internal: ())
 	}
-	
+
 	@usableFromInline
-	init(toAnalyticsData: @escaping (State, Action) -> AnalyticsData?, internal: Void) {
+	init(toAnalyticsData: @escaping (State, Action) -> [AnalyticsData]?, internal: Void) {
 		self.toAnalyticsData = toAnalyticsData
 	}
-	
+
 	@inlinable
 	public func reduce(into state: inout State, action: Action) -> Effect<Action> {
 		guard let analyticsData = toAnalyticsData(state, action) else {
 			return .none
 		}
 
-		return .run { _ in
-			analyticsClient.sendAnalytics(analyticsData)
-		}
+		return .concatenate(
+			analyticsData.map { data in
+				.run { _ in analyticsClient.sendAnalytics(data) }
+			}
+		)
 	}
 }
